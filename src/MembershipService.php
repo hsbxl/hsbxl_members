2a01b11b401c5d113ef239b7d062c7fa8fd063c3
betz <tom@behets.me>
2017-10-14 00:58:12 +0200

    Start memberships service

---
+++ src/MembershipService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\hsbxl_members;
+
+/**
+ * Class MembershipService.
+ */
+class MembershipService implements MembershipServiceInterface {
+
+  /**
+   * Constructs a new MembershipService object.
+   */
+  public function __construct() {
+
+  }
+
+}
---
+++ src/MembershipServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\hsbxl_members;
+
+/**
+ * Interface MembershipServiceInterface.
+ */
+interface MembershipServiceInterface {
+
+
+}
